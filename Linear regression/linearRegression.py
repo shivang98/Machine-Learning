17c1a13d060067ed4363c4bf5ae4261dbdd0af58
Shivang Agarwal <shivangagg98@gmail.com>
2017-10-24 07:33:27 +0530

    Update linearRegression.py

--- Linear regression/linearRegression.py
+++ Linear regression/linearRegression.py
@@ -28,7 +28,7 @@ regressor.fit(X_train, y_train)
 # predict
 y_pred = regressor.predict(X_test)
 
-# visualise
+# visualise training set
 plt.scatter(X_train,y_train,color = 'red')
 plt.plot(X_train,regressor.predict(X_train), color = 'blue')
 plt.title('Salary plot (training set)')
@@ -36,6 +36,7 @@ plt.xlabel('Employe experience')
 plt.ylabel('Salary')
 plt.show()
 
+# visualise testing set
 plt.scatter(X_test,y_test,color = 'red')
 plt.plot(X_train,regressor.predict(X_train), color = 'blue')
 plt.title('Salary plot (testing set)')
